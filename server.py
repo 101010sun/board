629c4453995bab83c61e25c117cdf685583fedc5
孫苡菱 <d0782838@mail.fcu.edu.tw>
2020-06-04 19:29:40 +0800

    排序

--- server.py
+++ server.py
@@ -183,7 +183,7 @@ def online_player():
         return None
 
 def show_year():
-    sql='SELECT DISTINCT 入隊學年 FROM 球員 LEFT JOIN 退休球員 USING(學號) WHERE 退休學年 is NULL'
+    sql='SELECT DISTINCT 入隊學年 FROM 球員 LEFT JOIN 退休球員 USING(學號) WHERE 退休學年 is NULL ORDER BY 入隊學年 ASC;'
     try:
         cursor.execute(sql)
         data = cursor.fetchall()
