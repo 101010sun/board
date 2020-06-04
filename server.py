c161f40131a01b072a6ae9ab9122e7a17284f579
孫苡菱 <d0782838@mail.fcu.edu.tw>
2020-06-05 04:39:35 +0800

    結合 53server

--- server.py
+++ server.py
@@ -208,7 +208,7 @@ def show_year():
     except:
         return None
 
-def new_data(name,stuid,num,in_year):
+def new_data(name,stuid,num,in_year): #53
     sql = "INSERT INTO 球員(名字, 學號, 背號, 入隊學年)VALUES(%s, %s, %s, %s)"
     try:
         cursor.execute(sql,(name,stuid,num,in_year))
@@ -216,7 +216,7 @@ def new_data(name,stuid,num,in_year):
     except: 
         conn.rollback()
 
-def fix_data(new_name,new_stuid,new_num,new_in_year,odd_name,odd_stuid,odd_num,odd_in_year):
+def fix_data(new_name,new_stuid,new_num,new_in_year,odd_name,odd_stuid,odd_num,odd_in_year): #53
     sql = "UPDATE 球員 SET 名字 = %s, 學號= %s ,背號 = %s,入隊學年= %s WHERE 球員.名字 = %s and 球員.學號 = %s and 球員.背號 = %s and 球員.入隊學年=%s "
     try:
         cursor.execute(sql,(new_name,new_stuid,new_num,new_in_year,odd_name,odd_stuid,odd_num,odd_in_year))
@@ -270,3 +270,25 @@ def player_performance(date,game_name,competitor_school,competitor_dept_name,Id)
     except:
         conn.rollback()
         return 0
+
+def new_game(date,game,oppschool,oppdep): #53
+    sql = "INSERT INTO 比賽( 日期, 盃賽名稱, 對手學校, 對手系名 )VALUES(%s, %s, %s, %s)"
+    try:
+        cursor.execute(sql,(date,game,oppschool,oppdep))
+    # 執行SQL语句
+    # 提交到資料庫系統執行
+        conn.commit()
+    except:
+   # 發生異常錯誤時回復
+        conn.rollback()
+
+def player_ingamedata(twopoint,twopointin,threepoint,threepointin,faball,faballin,backboard,goboard,gohelp,block,cut,mistake,foul,fouled): #53
+    sql = "INSERT INTO 表現(編號 ,二分球投, 二分球中, 三分球投, 三分球中, 罰球投, 罰球中, 防守籃板, 進攻籃板, 助攻, 阻攻, 抄截, 失誤, 犯規, 被犯)VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
+    try:
+        cursor.execute(sql,(NULL,twopoint,twopointin,threepoint,threepointin,faball,faballin,backboard,goboard,gohelp,block,cut,mistake,foul,fouled))
+    # 執行SQL语句
+    # 提交到資料庫系統執行
+        conn.commit()
+    except:
+   # 發生異常錯誤時回復
+        conn.rollback()
