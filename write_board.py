c006f96b29585d33dfcfbc64a00bb432455f12ab
孫苡菱 <d0782838@mail.fcu.edu.tw>
2020-06-04 17:12:51 +0800

    10

--- write_board.py
+++ write_board.py
@@ -4,22 +4,22 @@ import server
 
 wordfont= ('Arial', 12)
 classfont=('Arial', 18, "bold")
-menufont=('Arial', 14, "bold")
+menufont=('Arial', 10)
 
 class BoardApp(tk.Tk):
     def __init__(self):
         tk.Tk.__init__(self)
         self._frame = None
-        self.switch_frame(RecordBoard)
+        self.switch_frame(ShowRecordBoard)
 
     def switch_frame(self, frame_class):
         new_frame = frame_class(self)
         if self._frame is not None:
             self._frame.destroy()
         self._frame = new_frame
-        self._frame.pack()
+        self._frame.pack(fill='both')
 
-class RecordBoard(tk.Frame):
+class ShowRecordBoard(tk.Frame):
     def __init__(self,master):
         def page_playerdata(): #球員數據頁面 我打好了
             def clean_smallframe(): #清空小frame裡的物件
@@ -295,50 +295,7 @@ class RecordBoard(tk.Frame):
         def page_changedata():
             tk.Label(object_frame,text="修改資料", font=classfont).pack(side="top", fill="x", pady=5)
 
-        def page_startboard():
-            tk.Button(object_frame, text='開始記錄', width=20, height=10, command=lambda: [clean_frame(), page_boardgetgameinfo()]).pack()
-
-        def page_boardgetgameinfo(): #我打好了
-            def do_print():
-                print('%s' %(dateString.get()))
-                print('%s' %(gameString.get()))
-                print('%s' %(oppschoolString.get()))
-                print('%s' %(oppdepString.get()))
-                
-            tk.Label(object_frame, text="開始記錄", font=('Arial', 18, "bold")).grid(column=0, row=0, sticky=tk.W)
-            #Label-文字標籤
-            dateLabel = tk.Label(object_frame, text='日期:')
-            gameLabel = tk.Label(object_frame, text='盃賽名稱:')
-            oppschoolLabel = tk.Label(object_frame, text='對手學校')
-            oppdepLabel = tk.Label(object_frame, text='對手系名')
-
-            dateLabel.grid(column=0, row=1, sticky=tk.W)
-            gameLabel.grid(column=0, row=2, sticky=tk.W)
-            oppschoolLabel.grid(column=0, row=3, sticky=tk.W)
-            oppdepLabel.grid(column=0, row=4, sticky=tk.W)
-            #定義文字輸入框裡的文字物件
-            dateString = tk.StringVar()
-            gameString = tk.StringVar()
-            oppschoolString = tk.StringVar()
-            oppdepString = tk.StringVar()
-            dateEntry = tk.Entry(object_frame, show=None, font=('Arial', 14), textvariable=dateString)
-            gameEntry = tk.Entry(object_frame, show=None, font=('Arial', 14), textvariable=gameString)
-            oppschoolEntry = tk.Entry(object_frame, show=None, font=('Arial', 14), textvariable=oppschoolString)
-            oppdepEntry = tk.Entry(object_frame, show=None, font=('Arial', 14), textvariable=oppdepString)
-
-            dateEntry.grid(column=1, row=1, padx=10)
-            gameEntry.grid(column=1, row=2, padx=10)
-            oppschoolEntry.grid(column=1, row=3, padx=10)
-            oppdepEntry.grid(column=1, row=4, padx=10)
-
-            tk.Button(object_frame, text='確定', command=lambda: [clean_frame(), do_print(), page_boardchoosplayer()]).grid(column=1, row=5, sticky=tk.N)
-    
-        def page_boardchoosplayer(): #我打好了
-            data = server.online_player()
-            #print(data)
-
-        #清空object_frame裡面的東西
-        def clean_frame():
+        def clean_frame(): #清空object_frame裡面的東西
             for widget in object_frame.winfo_children():
                 widget.destroy()
             for widget in object_frame2.winfo_children():
@@ -346,7 +303,7 @@ class RecordBoard(tk.Frame):
 
         tk.Frame.__init__(self, master)
         menu_frame = tk.Frame(self)
-        menu_frame.pack(side=tk.TOP)
+        menu_frame.pack(side=tk.TOP, fill='x')
         funcmb = tk.Menubutton(menu_frame, text="模式", font=menufont, relief="flat")
         funcmb.grid(row=0, column=0)
         funcmenu = tk.Menu(funcmb,tearoff=0)
@@ -359,12 +316,10 @@ class RecordBoard(tk.Frame):
         rankmb = tk.Menubutton(menu_frame, text="排行", font=menufont, relief="flat")
         rankmb.grid(row=0, column=3)
         rankmenu = tk.Menu(rankmb,tearoff=0)
-
         object_frame = tk.Frame(self)
         object_frame.pack()
         object_frame2 = tk.Frame(self)
         object_frame2.pack()
-
         # 查詢選項的下拉式選單
         querymenu.add_command(label='球員數據', command=lambda: [clean_frame(), page_playerdata()])
         querymenu.add_command(label='球隊數據', command=lambda: [clean_frame(), page_teamdata()])
@@ -384,13 +339,86 @@ class RecordBoard(tk.Frame):
         setmenu.add_command(label='新增球員', command=lambda: [clean_frame(), page_newplayer()])
         setmenu.add_command(label='修改球員資料', command=lambda: [clean_frame(), page_changedata()])
         #模式選項的下拉式選單
-        funcmenu.add_command(label='記分板版', command=lambda: [clean_frame(), page_startboard()])
+        funcmenu.add_command(label='記分板版', command=lambda: [clean_frame(), master.switch_frame(RecordBoard)])
         
         funcmb.config(menu=funcmenu)
         setmb.config(menu=setmenu)
         querymb.config(menu=querymenu)
         rankmb.config(menu=rankmenu)
-    
+
+class RecordBoard(tk.Frame):
+    def __init__(self, master):
+        def clean_frame(): #清空object_frame裡面的東西
+            for widget in object_frame.winfo_children():
+                widget.destroy()
+            for widget in object_frame2.winfo_children():
+                widget.destroy()
+            for widget in object_frame3.winfo_children():
+                widget.destroy()
+        
+        def page_boardgetgameinfo():
+            clean_frame()
+            def do_print():
+                print('%s' %(dateString.get()))
+                print('%s' %(gameString.get()))
+                print('%s' %(oppschoolString.get()))
+                print('%s' %(oppdepString.get()))
+                
+            tk.Label(object_frame, text="開始記錄", font=classfont).pack(side='top')
+            tk.Label(object_frame, text="輸入比賽資訊", font=wordfont).pack(side='top')
+            #Label-文字標籤
+            dateLabel = tk.Label(object_frame2, text='日期:')
+            gameLabel = tk.Label(object_frame2, text='盃賽名稱:')
+            oppschoolLabel = tk.Label(object_frame2, text='對手學校')
+            oppdepLabel = tk.Label(object_frame2, text='對手系名')
+
+            dateLabel.grid(column=0, row=1, sticky=tk.W)
+            gameLabel.grid(column=0, row=2, sticky=tk.W)
+            oppschoolLabel.grid(column=0, row=3, sticky=tk.W)
+            oppdepLabel.grid(column=0, row=4, sticky=tk.W)
+            #定義文字輸入框裡的文字物件
+            dateString = tk.StringVar()
+            gameString = tk.StringVar()
+            oppschoolString = tk.StringVar()
+            oppdepString = tk.StringVar()
+            dateEntry = tk.Entry(object_frame2, show=None, font=('Arial', 14), textvariable=dateString)
+            gameEntry = tk.Entry(object_frame2, show=None, font=('Arial', 14), textvariable=gameString)
+            oppschoolEntry = tk.Entry(object_frame2, show=None, font=('Arial', 14), textvariable=oppschoolString)
+            oppdepEntry = tk.Entry(object_frame2, show=None, font=('Arial', 14), textvariable=oppdepString)
+
+            dateEntry.grid(column=1, row=1, padx=10)
+            gameEntry.grid(column=1, row=2, padx=10)
+            oppschoolEntry.grid(column=1, row=3, padx=10)
+            oppdepEntry.grid(column=1, row=4, padx=10)
+
+            tk.Button(object_frame3, text='確定', command=lambda: [clean_frame(), do_print(), page_boardchoosplayer()]).pack()
+
+        def page_boardchoosplayer(): #我打好了
+            clean_frame()
+            tk.Label(object_frame, text="選擇上場球員", font=('Arial', 18, "bold")).pack(side='top')
+            player_frame1 = tk.Frame(object_frame2)
+            player_frame1.pack()
+            player_frame2 = tk.Frame(object_frame2)
+            player_frame2.pack()
+            player_frame3 = tk.Frame(object_frame2)
+            player_frame3.pack()
+            player_frame4 = tk.Frame(object_frame2)
+            player_frame4.pack()
+            data = server.online_player()
+
+        tk.Frame.__init__(self, master)
+        close_frame = tk.Frame(self)
+        close_frame.pack(side='top',fill='x')
+        object_frame = tk.Frame(self)
+        object_frame.pack()
+        object_frame2 = tk.Frame(self)
+        object_frame2.pack()
+        object_frame3 = tk.Frame(self)
+        object_frame3.pack()
+        closebutton = tk.Button(close_frame, text='x', command=lambda: master.switch_frame(ShowRecordBoard)).pack(side='right')
+        page_boardgetgameinfo()
+        
+
 if __name__ == "__main__":
     window = BoardApp()
     window.geometry('1000x600')
